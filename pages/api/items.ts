640a021bffe047dd15e1ef969ca9f62af4c3a89c
Pawel Nackiewicz <it.pnackiewicz@gmail.com>
2021-11-12 20:11:50 +0100

    :wrench: add cors to api

---
+++ pages/api/items.ts
@@ -0,0 +1,21 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import Cors from "cors";
+import initMiddleware from "../../lib/init-middleware";
+
+const items: string[] = ["element a", "element b", "element c"];
+
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ["GET", "POST", "OPTIONS"],
+  })
+);
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Facility[]>
+) {
+  await cors(req, res);
+  res.status(200).json(items);
+}
